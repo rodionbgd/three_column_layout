57c19dd7c251b0f5cdfe6c799cb931cf004c53c8
rodionbgd <rodionbgd@mail.ru>
2021-10-16 21:50:38 +0300

    Создание трехколоночного макета.

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+};
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  ignorePatterns: ["**/dist/**"],
+};
